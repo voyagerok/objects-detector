d4704dc245ce16a212b62272bd46eb2d39b9f91b
Николай <shepard1010@gmail.com>
2016-06-05 21:42:35 +0300

    minor update

--- src/main.c
+++ src/main.c
@@ -8,17 +8,25 @@ int main(int argc, char **argv)
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new (app_id, G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "startup",
-                   G_CALLBACK(on_startup),
-                   NULL);
-  g_signal_connect(app, "activate",
-                   G_CALLBACK(on_activate),
-                   NULL);
-  g_signal_connect(app, "shutdown",
-                   G_CALLBACK(on_shutdown),
-                   NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
+  app = gtk_application_new (app_id,
+                             G_APPLICATION_FLAGS_NONE);
+
+  g_signal_connect (app,
+                    "startup",
+                    G_CALLBACK(on_startup),
+                    NULL);
+  g_signal_connect (app,
+                    "activate",
+                    G_CALLBACK(on_activate),
+                    NULL);
+  g_signal_connect (app,
+                    "shutdown",
+                    G_CALLBACK(on_shutdown),
+                    NULL);
+
+  status = g_application_run(G_APPLICATION(app),
+                             argc,
+                             argv);
 
   return status;
 }
